5ae7d3ee09df1fa67f63a054e675d4dd7210c9e9
brent hartshorn <brenthartshorn@proton.me>
2026-08-18 08:28:09 -0700

    make mathematica optional: fixes __all__

--- cymetric/pointgen/__init__.py
+++ cymetric/pointgen/__init__.py
@@ -8,21 +8,7 @@ using Mathematica as backend for numerical computations.
     Fabian Ruehle f.ruehle@northeastern.edu
 """
 
-# Optional backend: needs wolframclient, which is not a hard dependency.
-try:
-    from .pointgen_mathematica import PointGeneratorMathematica, ToricPointGeneratorMathematica
-except ImportError:  # pragma: no cover - optional
-    pass
-from .pointgen_cicy import CICYPointGenerator
-from .pointgen_equivariant import EquivariantCICYPointGenerator
-from .pointgen_toric import ToricPointGenerator
-from .pointgen import PointGenerator
-from .pointgen_mc import CICYPointGeneratorMC, ToricPointGeneratorMC
-from .nphelper import prepare_dataset, prepare_basis_pickle, get_levicivita_tensor
-
 __all__ = [
-    'PointGeneratorMathematica',
-    'ToricPointGeneratorMathematica',
     'CICYPointGenerator',
     'EquivariantCICYPointGenerator',
     'ToricPointGenerator',
@@ -33,3 +19,18 @@ __all__ = [
     'prepare_basis_pickle',
     'get_levicivita_tensor'
 ]
+
+# Optional backend: needs wolframclient, which is not a hard dependency.
+try:
+    from .pointgen_mathematica import (PointGeneratorMathematica, ToricPointGeneratorMathematica)
+except ImportError: # pragma: no cover - optional
+    pass
+else: __all__ += ['PointGeneratorMathematica', 'ToricPointGeneratorMathematica']
+
+from .pointgen_cicy import CICYPointGenerator
+from .pointgen_equivariant import EquivariantCICYPointGenerator
+from .pointgen_toric import ToricPointGenerator
+from .pointgen import PointGenerator
+from .pointgen_mc import CICYPointGeneratorMC, ToricPointGeneratorMC
+from .nphelper import prepare_dataset, prepare_basis_pickle, get_levicivita_tensor
+
